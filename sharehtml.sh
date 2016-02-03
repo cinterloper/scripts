1d1ab03823171c10ee6bce60901f06e70c574cac
grant haywood <grant@iowntheinter.net>
2016-02-03 05:35:04 +0000

    useful

--- sharehtml.sh
+++ sharehtml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CTR=$(docker -H localhost:4243 run -t -i -d -v $(pwd):/usr/share/nginx/html -P nginx )
-docker -H localhost:4243 inspect $CTR | jq .NetworkSettings.Ports
+CTR=$(docker -H localhost:4243 run -t -i -d -v "$(pwd)":/usr/share/nginx/html -v $(dirname $(realpath -s $0))/conf/nginx-dirlist.conf:/etc/nginx/conf.d/default.conf -P nginx )
+docker -H localhost:4243 inspect $CTR | jq .[].NetworkSettings.Ports
 
 
 
